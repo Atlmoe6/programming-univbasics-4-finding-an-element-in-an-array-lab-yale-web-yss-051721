6ea984227ab063a5f11a20ea816281a8d53b1ad7
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-28 08:18:11 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,9 @@ def find_element_index(array, value_to_find)
     count = 0 
 found_value_index = nil
     while count < array.size do
-    puts array [count] == value_to_find
-    puts found_value_index = count
+   puts array [count] == value_to_find
+   puts found_value_index = count
     count +=1
 end 
+found_value_index
 end
